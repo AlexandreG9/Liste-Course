841d2356a9b8454f37afaa131508a6c418ee1c75
AlexandreG9 <aguiheneuf@gmail.com>
2017-12-05 19:31:08 +0100

    Ajout du projet

---
+++ src/main/java/fr/aguiheneuf/ListeCourseApplication.java
@@ -0,0 +1,12 @@
+package fr.aguiheneuf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListeCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListeCourseApplication.class, args);
+	}
+}
